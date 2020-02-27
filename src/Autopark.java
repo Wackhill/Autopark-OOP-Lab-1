2554b98e14ca4a4c7c8e4f3c2f0c963bd05a52ce
Andrei <shakovskiyandrei@gmail.com>
2020-02-27 14:24:27 +0300

    Basic objects list is working now

--- src/Autopark.java
+++ src/Autopark.java
@@ -1,16 +1,16 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Autopark {
     private AutoparkItems autoparkItems = new AutoparkItems();
     private static GUIClass guiClass;
+    private static JButton selectedButton = null;
+
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -21,10 +21,42 @@ public class Autopark {
         });
 
         int curPos = 10;
+        ArrayList<Field> mainObjectsList = getMainObjects(new ArrayList<Field>(), AutoparkItems.class);
+        for (int i = 0; i < mainObjectsList.size(); i++) {
+            JButton selectObjectButton =
+                    new JButton(String.valueOf(classByField(mainObjectsList.get(i), AutoparkItems.class))
+                    .replace("class ", ""));//"✎"
+            selectObjectButton.setSize(150, 35);
+            selectObjectButton.setLocation(10, curPos);
+            selectObjectButton.setBackground(GUIClass.BASIC_BUTTON_COLOR);
+            curPos += 35;
+
+            guiClass.mainLayout.add(selectObjectButton);
+
+            int finalI = i;
+            selectObjectButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    if (selectedButton != null) {
+                        selectedButton.setBackground(GUIClass.BASIC_BUTTON_COLOR);
+                    }
+                    selectObjectButton.setBackground(GUIClass.SELECTED_BUTTON_COLOR);
+                    selectedButton = selectObjectButton;
+                }
+            });
+        }
+        guiClass.repaint();
+/*
         List fieldsList = getAllFields(new ArrayList<Field>(), AutoparkItems.class);
         for (int i = 0; i < fieldsList.size(); i++) {
             //System.out.println(fieldsList.get(i));
-            JButton jButton = new JButton(fieldsList.get(i).toString());
+            JButton jButton;
+            if (fieldsList.get(i).toString().contains("List")) {
+                jButton = new JButton("=====================");
+            }
+            else {
+                jButton = new JButton(fieldsList.get(i).toString());
+            }
             jButton.setSize(400, 35);
             jButton.setLocation(20, curPos);
             curPos += 30;
@@ -39,42 +71,35 @@ public class Autopark {
             });
         }
 
-        //Bus bus = new Bus(1, 2, 3, 4, Vehicle.GearboxType.AUTOMATIC, Vehicle.FuelType.ELECTRIC, 3, 5);
-        //System.out.println(bus.getWidth());
-
-            /*
-    private Route[] route = {new Route(1, 2, "Немига"),
-                             new Route(2, 3, "Купаловская"),
-                             new Route(4, 5, "Партизанская")};
-
-    ArrayList<String> categoriesList = new ArrayList<>();
-    */
+ */
     }
 
     public static List<Field> getAllFields(List<Field> fields, Class<?> type) {
         for (Field field : type.getDeclaredFields()) {
             String fieldType = field.getType().toString();
-            //System.out.println("Field type: " + fieldType);
-            //System.out.println(field.toString());
 
             if (fieldType.contains("List")) {
-                String listVar = field.toString().substring(field.toString().lastIndexOf('.') + 1);
+                String listVarName = field.toString().substring(field.toString().lastIndexOf('.') + 1);
 
-                Field stringListField = null;
+                Field listField = null;
                 try {
-                    stringListField = type.getDeclaredField(listVar);
+                    listField = type.getDeclaredField(listVarName);
                 } catch (NoSuchFieldException e) {
                     e.printStackTrace();
                 }
 
-                ParameterizedType stringListType = (ParameterizedType) stringListField.getGenericType();
-                Class<?> stringListClass = (Class<?>) stringListType.getActualTypeArguments()[0];
-                System.out.println(stringListClass);
-                //System.out.println("+++" + listVar + "+++");
-            }
+                ParameterizedType listType = (ParameterizedType) listField.getGenericType();
+                Class<?> listClass = (Class<?>) listType.getActualTypeArguments()[0];
+                System.out.println(listClass);
 
-            //fields.add(field);
+                getAllFields(fields, listClass);
+                //System.out.println(listClass); //Ternary operator booleanExpression ? expression1 : expression2
+            }
+            fields.add(field);//TODO:====================
+        }
             /*
+
+
             try {
                 if (isClass(Class.forName(fieldType.substring(fieldType.lastIndexOf(" ") + 1)))) {
                     System.out.println("OH, SHIT, I'M CLASSSS!!!!!!!");
@@ -87,10 +112,7 @@ public class Autopark {
 
             }
             else if (fieldType.contains("class") && !fieldType.contains("lang")) {
-
-
              */
-
                 /*
                 System.out.println("===== BAN =====");
                 try {
@@ -100,7 +122,6 @@ public class Autopark {
                 }
                  */
             //}
-        }
 
 //        fields.addAll(Arrays.asList(type.getDeclaredFields()));
 
@@ -127,6 +148,28 @@ public class Autopark {
         return ArrayList.class.isAssignableFrom(type);
     }
 
+    private static ArrayList<Field> getMainObjects(ArrayList<Field> fields, Class entryClass) {
+        for (Field field : entryClass.getDeclaredFields()) {
+            String fieldType = field.getType().toString();
+            if (fieldType.contains("List")) {
+                fields.add(field);
+            }
+        }
+        return fields;
+    }
+
+    private static Class classByField(Field field, Class fieldClass) {
+        String fieldName = field.toString().substring(field.toString().lastIndexOf('.') + 1);
+        Field listField = null;
+        try {
+            listField = fieldClass.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        ParameterizedType listType = (ParameterizedType) listField.getGenericType();
+        return (Class) listType.getActualTypeArguments()[0];
+    }
+
     /*
     public static List<Field> getAllFields(List<Field> fields, Class<?> type) {
         //fields.addAll(Arrays.asList(type.getDeclaredFields()));
@@ -148,4 +191,15 @@ public class Autopark {
     }
 
      */
+
+    //Bus bus = new Bus(1, 2, 3, 4, Vehicle.GearboxType.AUTOMATIC, Vehicle.FuelType.ELECTRIC, 3, 5);
+    //System.out.println(bus.getWidth());
+
+            /*
+    private Route[] route = {new Route(1, 2, "Немига"),
+                             new Route(2, 3, "Купаловская"),
+                             new Route(4, 5, "Партизанская")};
+
+    ArrayList<String> categoriesList = new ArrayList<>();
+    */
 }
--- src/GUIClass.java
+++ src/GUIClass.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUIClass extends JFrame {
-    public JPanel mainLayout;
+    public static final Color BASIC_BUTTON_COLOR = Color.decode("#f6f6f6");
+    public static final Color SELECTED_BUTTON_COLOR = Color.decode("#00a64b");
+    JPanel mainLayout;
 
-    public GUIClass() {
+    GUIClass() {
         super("Autopark");
-        setBounds(300, 50, 500, 760);
+        setBounds(300, 50, 700, 760);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainLayout = new JPanel();
         mainLayout.setLayout(null);
